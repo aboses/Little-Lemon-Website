68720d7881e249efaad122104169e5c4c303b6d2
aboses <arpitaboseshilpi@gmail.com>
2025-06-26 09:59:05 +0530

    menu

--- llw/src/App.js
+++ llw/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Nav from './Components/Nav';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
+import Menu from './Components/Menu';
 
 function App(){
   return (
     <>
       <Nav/>
       <Header/>
+      <Menu/>
       <Footer/>
     </>
   )
--- llw/src/Components/Menu.js
+++ llw/src/Components/Menu.js
@@ -1,4 +1,33 @@
 import React from "react";
+import recipes from "../Recipes";
 
 
 
+const Main = () => {
+    return (
+        <div className="menu-container">
+           <div className="menu-header">
+             <h2>This Weeks Specials!</h2>
+             <a href="/Menu"><button aria-label="On Click">Online Menu</button></a>
+           </div>
+
+           <div className="cards">
+              {recipes.map((recipe) => (
+                <div key={recipe.id} className="menu-items">
+                  <img src={recipe.image} alt="" />
+                  <div className="menu-content">
+                    <div className="heading">
+                      <h5>{recipe.title}</h5>
+                      <p>${recipe.price}</p>
+                    </div>
+                    <p>{recipe.description}</p>
+                    <button className="orderbtn" onClick={() => (recipe.id)}>Order Now</button>
+                  </div>
+                </div>
+              ))}
+           </div>
+        </div>
+    )
+};
+
+export default Main;
